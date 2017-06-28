f050d0e66f7bc2ea9f943d58ba4e47a0f493157c
acharp <arnaudcharpentier91@gmail.com>
2017-06-28 15:23:39 +0200

    Support mapping response headers

--- tests/integration/my_pckg/api.py
+++ tests/integration/my_pckg/api.py
@@ -3,14 +3,26 @@
 
 def get(event, *args, **kwargs):
     """Test get path."""
-    return {"body": "Get has been called"}
+    return {
+            "statusCode": 200,
+            "headers": {"response_status": "ok"},
+            "body": "Get has been called"
+           }
 
 
 def put(event, *args, **kwargs):
     """Test put path."""
-    return {"body": "Put has been called"}
+    return {
+            "statusCode": 544,
+            "headers": {"response_status": "YOLO"},
+            "body": "Put has been called"
+           }
 
 
 def delete(event, *args, **kwargs):
     """Test delete path."""
-    return {"body": "Delete has been called"}
+    return {
+            "statusCode": 504,
+            "headers": {"yolo_header": "timeout"},
+            "body": "Delete has been called"
+           }
